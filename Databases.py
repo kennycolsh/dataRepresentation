9fac1d850b1c93a8858831daa8a9e2f58aad3bdc
Kenny Colsh <kennycolsh@yahoo.com>
2019-11-30 15:58:38 +0000

    Created Class for DB

--- Databases.py
+++ Databases.py
@@ -1,15 +1,56 @@
 import mysql.connector
 
-db = mysql.connector.connect(
-    host ="localhost", user="root",
-    password="",database="popcars"
-)
+class CarsDAO:
+    db=""
+    def __init__(self):
+        self.db = mysql.connector.connect(
+        host ="localhost", user="root",
+        password="",database="popcars"
+        )
+
+    def create(self,values):
+        cursor = self.db.cursor()
+        sql="insert into cars (reg,make,model,price,totalvotes) values (%s,%s,%s,%s,%s)"
+        cursor.execute(sql, values)
+        self.db.commit()
+        return cursor.lastrowid
+
+
+    def update(self,values):
+        cursor = self.db.cursor()
+        sql="update cars set reg =%s,make =%s,model =%s,price =%s,totalvotes =%s"
+        cursor.execute(sql, values)
+        self.db.commit()
+
+    def getall(self):
+        cursor = self.db.cursor()
+        sql="select * from cars"
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        return result
+
+    def findbyid(self,id):
+        cursor = self.db.cursor()
+        sql="select * from cars where id = %s"
+        values = (id,)
+        cursor.execute(sql, values)
+        result = cursor.fetchone()
+        return result
+
+    def delete(self,id):
+        cursor = self.db.cursor()
+        sql="DELETE from cars where id= %s"
+        values = (id,)
+        cursor.execute(sql, values)
+        self.db.commit()
+        print("delete done")
+
+carsDAO = CarsDAO()
 
 
 
-cursor = db.cursor()
-sql="insert into cars (reg,make,model,price,totalvotes) values (%s,%s,%s,%s,%s)"
-values = ("121C1547","AUDI","A4",12501, 4)
-cursor.execute(sql, values)
-db.commit()
-print("1 record inserted, ID:", cursor.lastrowid)
+
+
+
+
+
